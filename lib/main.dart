d5bd2c39e822626b2ab8c6c4a09a045c512183b2
Rakhulant <rakrah51@gmail.com>
2020-10-27 19:54:03 +0530

    Colors

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,19 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.black),
+      theme: ThemeData(
+          primaryColor: Color(0xFF304ffe), accentColor: Color(0xFFff3d00)),
       home: Scaffold(
         appBar: AppBar(
           title: Text(
             'AppName',
             style: TextStyle(),
+          ),
+        ),
+        body: Center(
+          child: MaterialButton(
+            onPressed: () {},
+            child: Text('SignIn'),
           ),
         ),
       ),
