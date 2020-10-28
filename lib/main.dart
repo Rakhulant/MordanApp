ddd3008f932a06600a2a4a6d2f07d8c2e334cf9c
Rakhulant <rakrah51@gmail.com>
2020-10-28 12:37:50 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<Widget> pages = [
+    Home(),
+  ];
+  var cIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-            primaryColor: Color(0xFF69f0ae),
-            accentColor: Color(0xFFb0bec5),
-            backgroundColor: Color(0xFF2979ff),
-            buttonColor: Color(0xFFff3d00)),
-        home: Home());
+      theme: ThemeData(
+          primaryColor: Color(0xFF69f0ae),
+          accentColor: Color(0xFFb0bec5),
+          backgroundColor: Color(0xFF2979ff),
+          buttonColor: Color(0xFFff3d00)),
+      home: pages[cIndex],
+    );
   }
 }
